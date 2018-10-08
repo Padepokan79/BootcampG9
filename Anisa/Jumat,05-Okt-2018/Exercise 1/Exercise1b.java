74714acb0a659083fd46ea9a266382d19fb38abc
anissalf <anissafebriyanti05@gmail.com>
2018-10-08 09:22:36 +0700

    Add Exercise 1,2, dan 4

---
+++ Anisa/Jumat,05-Okt-2018/Exercise 1/Exercise1b.java
@@ -0,0 +1,6 @@
+public class Exercise1b{
+	public static void main (String[] args){
+		System.out.println("Anissa determined to learn ho to code");
+		System.out.println("Today's date is 5th October 2018");
+	}
+}
---
+++ Anisa/Jumat,05-Okt-2018/Exercise 2/Exercise2a.java
@@ -0,0 +1,25 @@
+public class Exercise2a{
+	public static void main (String[] args){
+		System.out.println("+------------------------------------------------+");
+		System.out.println("|                                     LINK       |");
+		System.out.println("|                                                |");
+	    System.out.println("| TANGGAL  : 05/10/2018         WAKTU  : 20:31:59|");
+		System.out.println("| ATM ID   : 450381             NO.REF : 03155   |");
+		System.out.println("| LOKASI   : 9839-YOMART GUNUNG BATU             |");
+		System.out.println("| NO.KARTU : 601301..9332                        |");
+		System.out.println("|                                                |");
+		System.out.println("|                TARIK TUNAI (BRI)               |");
+		System.out.println("|                                                |");
+		System.out.println("| JUMLAH  : RP.100.000                           |");
+		System.out.println("| SALDO   : RP.462.307                           |");
+		System.out.println("|                                                |");
+		System.out.println("|                                                |");
+		System.out.println("|:::::::::::::::: TRANSAKSI TUNAI :::::::::::::::|");
+		System.out.println("|                                                |");
+		System.out.println("|                                                |");
+		System.out.println("|                                                |");
+		System.out.println("+------------------------------------------------+");
+
+
+	}
+}
