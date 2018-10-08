d6acbf8e7fb0932596175881ee9b2c41e79188a7
TopanGunantara <gunantarat@gmail.com>
2018-10-08 09:15:37 +0700

    Add Exercise 1,2,4a

---
+++ Topan/5-10-18/Exercise1/Exercise1b.java
@@ -0,0 +1,6 @@
+public class Exercise1b{
+	public static void main(String[] args) {
+		System.out.println("I, Topan Gunantara, am determined to learn how to code.");
+		System.out.println("Today's date is Friday, October 22, 2018");
+	}
+}
---
+++ Topan/5-10-18/Exercise2/Exercise2a.java
@@ -0,0 +1,30 @@
+public class Exercise2a{
+	public static void main(String[] args) {
+		System.out.println(" ______________________________________ ");
+		System.out.println("|                                      |");
+		System.out.println("|   __\\\\\\ PERTAMINA         Pasti Pas! |");
+		System.out.println("|  //////                              |");
+		System.out.println("| ///                                  |");
+		System.out.println("|                                      |");
+		System.out.println("| SPBU 44.521.14                       |");
+		System.out.println("| JL. II ALTERNATIVE TEGAL - ADI WERNA |");
+		System.out.println("| TELP. 0283 - 3317422 TEGAL           |");
+		System.out.println("|                                      |");
+		System.out.println("| Minggu, 02 Februari 2014    12:37:03 |");
+		System.out.println("| ==================================== |");
+		System.out.println("| No. Struk     : 11                   |");
+		System.out.println("| Nomor Pompa   : 2                    |");
+		System.out.println("| Jenis BBM     : Premium              |");
+		System.out.println("| Liter         :                4,615 |");
+		System.out.println("| Harga/Liter   : Rp.            6.500 |");
+		System.out.println("| Total         : Rp.           30.000 |");
+		System.out.println("| ==================================== |");
+		System.out.println("|                                      |");
+		System.out.println("|    TERIMA KASIH DAN SELAMAT JALAN    |");
+		System.out.println("|    PREMIUM ADALAH BBM BERSUBSIDI     |");
+		System.out.println("|   HANYA UNTUK GOLONGAN TIDAK MAMPU   |");
+		System.out.println("|                                      |");
+		System.out.println("|______________________________________|");
+		System.out.println("");
+	}
+}
