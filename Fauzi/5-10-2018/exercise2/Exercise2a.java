4876e1375501130759b33292fdd50028d0ded701
CacingTamvan <syamsulmaarif.atp@gmail.com>
2018-10-08 09:15:15 +0700

    buka log5-10-2018.txt

---
+++ Fauzi/5-10-2018/exercise2/Exercise2a.java
@@ -0,0 +1,24 @@
+public class Exercise2a{
+	public static void main(String[] arg){
+		System.out.println("");
+		System.out.println("   // Bank Rakyat Indonesia    ");
+		System.out.println("   \\\\ Mengutamakan Kepuasan nasabah");
+		System.out.println("");
+		System.out.println("   PT BANK RAKYAT INDONESIA (PERSERO)");
+		System.out.println("   24/04/2013   19:14:20");
+		System.out.println("   094027 9827 - SPBU SOREANG   1071288");
+		System.out.println("   NO. KARTU:6..6711");
+		System.out.println("   TRANSFER ATM");
+		System.out.println("");
+		System.out.println("   DARI REKENING. : 054401008342507");
+		System.out.println("   KE REK.        : 068701000277516");
+		System.out.println("   NAMA           : YORISAN PERMANA BA");
+		System.out.println("   JUMLAH         : RP.        65.425");
+		System.out.println("");
+		System.out.println("   KARTU BRI DAPAT DIGUNAKAN BELANJA");
+		System.out.println("     DI TEMPAT BERLOGO MAESTRO DAN");
+		System.out.println("   TARIK TUNAI DI ATM BERLOGO CIRRUS");
+		System.out.println("");
+		System.out.println("       CALL BRI : 14017 / 6000017");
+	}
+}
