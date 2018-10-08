51db77e99e70859a7ac95badb3ef3600ae962ab1
eriksutiawan <eriksutiawan97@gmail.com>
2018-10-08 09:30:20 +0700

    Tugas Pelatihan Java : Exercise , Exercise1 , Exercise1A , Exercise1B , Exercise2 , Exercise2A , Exercise2B

---
+++ Erik/2018-10-05/Exercise1B/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg{
+	public static void main( String[] args ){
+		System.out.println("I, erik Sutiawan, am determined to learn how to code.");
+		System.out.println("Today's date is Friday, Oktober 05, 2018.");
+	}
+}
