935ec671e6a8ab0ef1b895fe9407d7517daeba97
WawanSetiawan1210 <wawansetia1210@gmail.com>
2018-10-08 09:20:38 +0700

    Ini commit Exercise 1 2 & 4

---
+++ Wawan/mulai-latihan/Exercise2/Exercise 2A/Exercise2A.java
@@ -0,0 +1,24 @@
+public class Exercise2A{
+	public static void main(String args[]){
+
+		System.out.println("+--------------------------------+");
+		System.out.println("|      >>>>>PERTAMINA<<<<<       |");
+		System.out.println("|          SPBU UNP 192          |");
+		System.out.println("|      Dakota No 8A Bandung      |");
+		System.out.println("|--------------------------------|");
+		System.out.println("|                                |");
+		System.out.println("| Jum'at, 5-10-2018  10:09AM     |");
+		System.out.println("| Nomor Nota     : 12834733      |");
+		System.out.println("| Jenis BBM      : Pertamax      |");
+		System.out.println("| Liter          : 3 L           |");
+		System.out.println("| Harga/Liter    : Rp.    8.000  |");
+		System.out.println("| Total          : Rp.   24.000  |");
+		System.out.println("|                                |");
+		System.out.println("|================================|");
+		System.out.println("|Terima kasih dan selamat jalan. |");
+		System.out.println("|                                |");
+		System.out.println("+--------------------------------+");
+		
+		
+	}
+}
---
+++ Wawan/mulai-latihan/Exercise2/GasolinReceipt.java
@@ -0,0 +1,19 @@
+public class GasolinReceipt{
+	public static void main(String args[]){
+
+		System.out.println("+--------------------------------+");
+		System.out.println("|                                |");
+		System.out.println("|          CORNER STORE          |");
+		System.out.println("|                                |");
+		System.out.println("|     2018,5 October  09:44AM    |");
+		System.out.println("|                                |");
+		System.out.println("| Gallons        : 10.870        |");
+		System.out.println("| Price/Gallon   : $2.089        |");
+		System.out.println("|                                |");
+		System.out.println("| Fuel Total     : $22.71        |");
+		System.out.println("|                                |");
+		System.out.println("+--------------------------------+");
+		
+		
+	}
+}
